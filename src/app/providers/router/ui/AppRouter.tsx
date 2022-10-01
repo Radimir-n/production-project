a340abade8cbf13822a0af6a411ddb9fde0ffb22
radmir.nafikov <radimir.nafikov@x5.ru>
2022-10-01 11:04:06 +0300

    add not found page and loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routerConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/ui/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>Загрузка...</div>}>
+    <Suspense fallback={<PageLoader />}>
         <Routes>
             {routerConfig.map(({ path, element }) => (
                 <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
