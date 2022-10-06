5c928a5be3a6436214056e05b45785865f5a0fb0
radmir.nafikov <radimir.nafikov@x5.ru>
2022-10-06 12:16:09 +0300

    change folder names

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,8 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
+import { Sidebar } from 'widgets/Sidebar';
 
 export function App() {
     const { theme } = useTheme();
