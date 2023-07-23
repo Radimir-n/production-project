04b855426a366b44cab272da99bf44771e7c9e89
radimir <radimirn@mail.ru>
2023-07-23 13:56:14 +0500

    add copy plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,25 +4,27 @@ import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPath } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
-    const paths: BuildPath = {
-        entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        build: path.resolve(__dirname, 'build'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
-        src: path.resolve(__dirname, 'src'),
-    };
-    const mode = env.mode || 'development';
-    const PORT = env.port || 3000;
-    const apiUrl = env.apiUrl || 'http://localhost:8000';
+  const paths: BuildPath = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
+    locales: path.resolve(__dirname, 'public', 'locales'),
+    buildLocales: path.resolve(__dirname, 'build', 'locales'),
+  };
+  const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+  const apiUrl = env.apiUrl || 'http://localhost:8000';
 
-    const isDev = mode === 'development';
+  const isDev = mode === 'development';
 
-    const config: webpack.Configuration = buildWebpackConfig({
-        mode,
-        paths,
-        isDev,
-        port: PORT,
-        apiUrl,
-        project: 'frontend',
-    });
-    return config;
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+    apiUrl,
+    project: 'frontend',
+  });
+  return config;
 };
