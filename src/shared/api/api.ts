0c12b9b249817e9a1ad59cd1efe02ef99d6ec3cf
radimir <radimirn@mail.ru>
2023-07-23 14:26:16 +0500

    fix api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,12 @@
 import { USER_LOCALSTORAGE_KEY } from 'shared/cost/localeStorage';
 
 export const $api = axios.create({
-    baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
-    },
+  baseURL: __API__,
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  }
+  return config;
 });
